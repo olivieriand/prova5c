42a3928a14549e71f9368f054e0d97c4ffd43a8c
andrea olivieri <andre.olly2005@gmail.com>
2023-11-10 12:17:26 +0100

    seconda modifica file

--- provagitolivieri/src/provagitolivieri/start.java
+++ provagitolivieri/src/provagitolivieri/start.java
@@ -5,7 +5,9 @@ public class start
 
 	public static void main(String[] args) 
 	{
-		system.out.println("ciao");
+		System.out.println("ciao");
+		
+		System.out.println("ciao sono eclipse");
 		
 
 	}
